fb7d411343594c09a434ee569e60ae25271020d8
georgejeffery <georgejeffery@gmail.com>
2019-02-12 09:41:48 +0000

    regenerated files

--- you_gi_oh_no/app/channels/game_channel.rb
+++ you_gi_oh_no/app/channels/game_channel.rb
@@ -2,10 +2,21 @@ class GameChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     stream_from "player_#{uuid}"
+    Match.create(uuid)
 
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    Game.withdraw(uuid)
+    # Remove yourself from the waiting list
+    Match.remove(uuid)
+  end
+
+  def take_turn(data)
+    Game.take_turn(uuid, data)
+  end
+
+  def new_game()
+    Game.new_game(uuid)
   end
 end
